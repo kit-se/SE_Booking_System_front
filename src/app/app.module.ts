099e059d61dc98f3a4bcb0f74fec22251fbac9f1
grapgrap <fussaep@gmail.com>
2017-07-10 17:02:40 +0900

    init project
    config scss lib
    intsall arrive.js for scss lib
    
    config tslint indent size space 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+}
