288f59d18b7ca85988467792b496c63765986271
grapgrap <fussaep@gmail.com>
2017-07-17 10:46:00 +0900

    main module init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MainModule } from './main/main.module';
 
 @NgModule({
     declarations: [
@@ -11,6 +12,8 @@ import { AppComponent } from './app.component';
     imports: [
         BrowserModule,
         AppRoutingModule,
+
+        MainModule
     ],
     providers: [],
     bootstrap: [ AppComponent ]
