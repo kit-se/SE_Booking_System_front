73724830de9b0e48f5a3bc5d2fa5fbd1af176539
grapgrap <fussaep@gmail.com>
2017-07-20 14:29:56 +0900

    리포트 현황 페이지 작성

--- src/app/admin/main/main-routing.module.ts
+++ src/app/admin/main/main-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import {MainComponent} from './main.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main.component';
 
 const routes: Routes = [
-  {path: '', component: MainComponent},
+    { path: '', component: MainComponent },
+    { path: 'report', loadChildren: 'app/admin/report/report.module#ReportModule' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forChild(routes) ],
+    exports: [ RouterModule ]
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
