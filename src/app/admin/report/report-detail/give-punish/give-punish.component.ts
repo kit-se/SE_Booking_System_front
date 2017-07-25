c44cb30736ba16413d0bba3ea501bdb37210e76d
grapgrap <fussaep@gmail.com>
2017-07-25 17:48:36 +0900

    제재 주는 기능 추가

---
+++ src/app/admin/report/report-detail/give-punish/give-punish.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-give-punish',
+  templateUrl: './give-punish.component.html',
+  styleUrls: ['./give-punish.component.scss']
+})
+export class GivePunishComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/admin/report/report-detail/give-punish/give-punish.module.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { GivePunishComponent } from './give-punish.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [ GivePunishComponent ],
+    exports: [ GivePunishComponent ]
+})
+export class GivePunishModule {
+}
