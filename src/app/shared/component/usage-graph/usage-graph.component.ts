1502f7d16e2aa2c393d70b5e18ea6e06414e06f3
grapgrap <fussaep@gmail.com>
2017-07-25 14:26:37 +0900

    예약된 시간과, 예약할 시간 분리

--- src/app/shared/component/usage-graph/usage-graph.component.ts
+++ src/app/shared/component/usage-graph/usage-graph.component.ts
@@ -6,32 +6,48 @@ import { Router } from '@angular/router';
     styleUrls: [ './usage-graph.component.scss' ]
 })
 export class UsageGraphComponent implements OnInit {
-    @Output() selected = new EventEmitter<string>();
+    @Output() selected = new EventEmitter<string>(); // 이 섹션에서 예약 중임을 부모 컴포넌트에 알림
+    @Input() disabled: boolean; // 다른 섹션 예약 중일 때 예약 방지 목적
     @Input() title: string;
-    @Input() disabled: boolean;
-    timeTable = [];
-    selectedTime = [];
+    timeTable: boolean[] = []; // 전체 시간 테이블
+    bookedTimeTable: boolean[] = []; // 예약된 시간 테이블
+    selectedTime: number[] = [];
 
     constructor (private router: Router) {
     }
 
     ngOnInit () {
+        // init variable.
         this.timeTable = new Array(24);
+        this.timeTable.fill( false );
+
+        // todo 서버에서 예약된 데이터를 불러오기
+        if ( this.title === 'A1') {
+            this.bookedTimeTable = [ false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false ];
+            this.timeTable = this.bookedTimeTable.slice(0); // 일반 복사를 수행하면 참조값이 넘어가기 때문에 깊은 복사를 수행한다.
+        } else if ( this.title === 'B1' ) {
+            this.bookedTimeTable = [ false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false ];
+            this.timeTable = this.bookedTimeTable.slice(0);
+        }
     }
 
     selectBookingTime (time: number) {
-        if ( this.disabled ) {
-            return;
-        }
-        if ( this.selectedTime.length === 0 ) { // 처음 시간을 선택하는 경우
-            this.selected.emit(this.title);
-        }
-        if ( this.timeTable[ time ] ) {
-            this.timeTable[ time ] = false;
-            this.selectedTime.splice( this.selectedTime.indexOf(time), 1); // time 찾아서 삭제.
-        } else {
-            this.timeTable[ time ] = true;
-            this.selectedTime.push(time);
+        if ( !this.disabled ) { // 다른 섹션을 예약 중이지 않을 때
+            if ( this.selectedTime.length === 0 ) { // 첫 시간을 클릭할 때 다른 섹션 예약 방지
+                this.selected.emit(this.title);
+            }
+            if ( this.timeTable[ time ] ) { // 시간 선택 취소
+                if ( !this.bookedTimeTable[ time ] ) { // 내가 선택한 시간이라면,
+                    this.timeTable[ time ] = false;
+                    this.selectedTime.splice( this.selectedTime.indexOf(time), 1); // time 찾아서 삭제.
+                    if ( this.selectedTime.length === 0 ) { // 취소해서 시간이 모두 없어지면 다른 섹션에 예약 할 수 있도록 함.
+                        this.selected.emit('');
+                    }
+                }
+            } else { // 시간 선택
+                this.timeTable[ time ] = true;
+                this.selectedTime.push(time);
+            }
         }
     }
 }
