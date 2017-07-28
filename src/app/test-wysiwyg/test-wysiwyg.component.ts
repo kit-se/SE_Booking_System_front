95392b8b00051927abf33b9781eedaee85e896eb
grapgrap <fussaep@gmail.com>
2017-07-28 13:37:44 +0900

    이미지 첨부, 프리뷰 완성

--- src/app/test-wysiwyg/test-wysiwyg.component.ts
+++ src/app/test-wysiwyg/test-wysiwyg.component.ts
@@ -1,25 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
+
 @Component({
     selector: 'app-test-wysiwyg',
     templateUrl: './test-wysiwyg.component.html',
     styleUrls: [ './test-wysiwyg.component.scss' ]
 })
 export class TestWysiwygComponent implements OnInit {
+    fileList: File[];
+    previewList: any[];
+    code: string;
+
     constructor () {
     }
+
     ngOnInit () {
+        this.fileList = [];
+        this.previewList = [];
+
         $('#summernote').summernote({
             height: 300,
             toolbar: [
-                ['insert', ['picture']]
-            ]
+                [ 'insert', [ 'picture' ] ]
+            ],
+            callbacks: {
+                onImageUpload: (files) => {
+                    for ( let i = 0; i < files.length; i++ ) {
+                        this.fileList.push(files[ i ]);
+                        this.makePreview(files[ i ]);
+                    }
+                }
+            }
         });
     }
 
-    public submit() {
-        const code = $('#summernote').summernote('code');
-        console.log( code );
+    private makePreview (file: File) {
+        const reader: FileReader = new FileReader();
+        reader.addEventListener('load', () => {
+            this.previewList.push(reader.result);
+        });
+
+        reader.readAsDataURL(file);
+    }
+
+    public submit () {
+        this.code = $('#summernote').summernote('code');
+        console.log(this.fileList);
     }
 }
--- src/app/test-wysiwyg/test-wysiwyg.module.ts
+++ src/app/test-wysiwyg/test-wysiwyg.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TestWysiwygComponent } from './test-wysiwyg.component';
+import { NavbarModule } from '../shared/component/navbar/navbar.module';
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        NavbarModule
     ],
     declarations: [ TestWysiwygComponent ]
 })
