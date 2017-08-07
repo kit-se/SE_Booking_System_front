a751045d19d0243144cb6356d9147fe12ba2989c
grapgrap <fussaep@gmail.com>
2017-08-07 14:02:27 +0900

    사용 오픈소스 라우터 추가

--- src/app/shared/component/footer/footer.module.ts
+++ src/app/shared/component/footer/footer.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        RouterModule
     ],
     declarations: [ FooterComponent ],
     exports: [ FooterComponent ]
