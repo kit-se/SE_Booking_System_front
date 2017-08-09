ba405b732fdc6f9dd7619be4af93a2909bc3cde2
grapgrap <fussaep@gmail.com>
2017-08-09 11:44:34 +0900

    시간 정보 불러오는 위치 변경

--- src/app/main/main-dashboard/main-dashboard.component.ts
+++ src/app/main/main-dashboard/main-dashboard.component.ts
@@ -4,15 +4,16 @@ import * as moment from 'moment';
 @Component({
     selector: 'app-main-dashboard',
     templateUrl: './main-dashboard.component.html',
-    styleUrls: [ './main-dashboard.component.scss' ]
+    styleUrls: ['./main-dashboard.component.scss']
 })
 export class MainDashboardComponent implements OnInit {
+    // 예약 일자 관련 변수
     dateFlag: string;
     date: string;
-
-    sectionList: string[];
+    // 섹션 관련 변수
     selectedSection: string;
-
+    // 예약 시간 관련 변수
+    bookingInfoList: any[];
     selectedTime: number[];
 
     constructor () {
@@ -22,11 +23,40 @@ export class MainDashboardComponent implements OnInit {
         this.dateFlag = 'today';
         this.date = moment().format('YY. MM. DD');
 
-        this.sectionList = ['A1', 'A2', 'A3', 'B1', 'B2'];
         this.selectedSection = '';
+
+        // 서버에서 오늘, 내일에 대한 예약 정보를 모두 불러온다
+        // todo 오늘, 내일에 대한 예약 정보를 분리해서, 해당 날짜 클릭시에 데이터를 바꿔준다.
+        this.bookingInfoList = [
+            {
+                section: 'A1',
+                date: moment().format('YYYY-MM-DD'),
+                bookedTime: [false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false]
+            },
+            {
+                section: 'A2',
+                date: moment().format('YYYY-MM-DD'),
+                bookedTime: [false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false]
+            },
+            {
+                section: 'A3',
+                date: moment().format('YYYY-MM-DD'),
+                bookedTime: [false, false, true, true, true, true, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false]
+            },
+            {
+                section: 'B1',
+                date: moment().format('YYYY-MM-DD'),
+                bookedTime: [false, false, false, false, false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false]
+            },
+            {
+                section: 'B2',
+                date: moment().format('YYYY-MM-DD'),
+                bookedTime: [false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false]
+            }
+        ]
     }
 
-    public switchDate( date: string ) {
+    public switchDate (date: string) {
         this.dateFlag = date;
         if ( this.dateFlag === 'today' ) {
             this.date = moment().format('YY. MM. DD');
@@ -35,7 +65,7 @@ export class MainDashboardComponent implements OnInit {
         }
     }
 
-    public book() {
+    public book () {
         // todo 서버로 예약 전송
         if ( this.selectedSection !== '' ) { // 선택한 섹션이 존재해야지 예약 가능
             console.log(this.selectedSection, sessionStorage.getItem('id'), this.selectedTime);
--- src/app/shared/component/usage-graph/usage-graph.component.ts
+++ src/app/shared/component/usage-graph/usage-graph.component.ts
@@ -11,9 +11,9 @@ export class UsageGraphComponent implements OnInit {
     @Output() selectedTimeOutput = new EventEmitter<number[]>(); // 예약 버튼 클릭시 현재까지 선택된 시간 배열을 부모 컴포넌트에 알림
     @Input() disabled: boolean; // 다른 섹션 예약 중일 때 예약 방지 목적
     @Input() section: string;
+    @Input() bookedTimeTable: boolean[];
     timeTable: number[] = [];
     bookingTable: boolean[] = []; // 전체 예약 테이블
-    bookedTimeTable: boolean[] = []; // 예약된 시간 테이블
     selectedTime: number[] = [];
 
     constructor (private router: Router) {
@@ -23,16 +23,7 @@ export class UsageGraphComponent implements OnInit {
         // init variable.
         this.timeTable = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4, 5];
         this.bookingTable = new Array(24);
-        this.bookingTable.fill(false);
-
-        // todo 서버에서 예약된 데이터를 불러오기
-        if ( this.section === 'A1' ) {
-            this.bookedTimeTable = [false, false, false, false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false];
-            this.bookingTable = this.bookedTimeTable.slice(0); // 일반 복사를 수행하면 참조값이 넘어가기 때문에 깊은 복사를 수행한다.
-        } else if ( this.section === 'B1' ) {
-            this.bookedTimeTable = [false, false, false, false, false, false, true, true, true, true, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
-            this.bookingTable = this.bookedTimeTable.slice(0);
-        }
+        this.bookingTable = this.bookedTimeTable.slice(0);
     }
 
     public selectBookingTime (time: number) {
