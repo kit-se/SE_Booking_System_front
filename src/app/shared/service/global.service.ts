40ba455321f2430204215335328dc7818531b458
grapgrap <fussaep@gmail.com>
2017-08-10 15:58:45 +0900

    end point 수정

--- src/app/shared/service/global.service.ts
+++ src/app/shared/service/global.service.ts
@@ -5,8 +5,8 @@ export class GlobalService {
     url: string;
 
     constructor () {
-        // this.url = 'http://13.124.177.198:3000';
-        this.url = 'http://localhost:3000';
+        this.url = 'http://13.124.177.198:3000';
+        //this.url = 'http://localhost:3000';
     }
 
 }
