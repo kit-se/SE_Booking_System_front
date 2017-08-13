8534b970572164fc4178231a9d272cad6d63641d
zu0p <a33a66a99@naver.com>
2017-08-13 17:15:55 +0900

    예약취소, 사용종료

--- src/app/mypage/mypage.component.ts
+++ src/app/mypage/mypage.component.ts
@@ -1,11 +1,16 @@
+import { BOOL_TYPE } from '@angular/compiler/src/output/output_ast';
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+
 
 @Component({
   selector: 'app-mypage',
   templateUrl: './mypage.component.html',
   styleUrls: ['./mypage.component.scss']
 })
+
 export class MypageComponent implements OnInit {
+  isBefore:boolean = moment('2017-08-01').isBefore('2017-08-02');
 
   constructor() { }
 
@@ -13,3 +18,4 @@ export class MypageComponent implements OnInit {
   }
 
 }
+
