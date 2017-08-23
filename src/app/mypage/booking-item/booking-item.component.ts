75fe12121ee304dc68156d872a309def3ab347ba
zu0p <a33a66a99@naver.com>
2017-08-23 18:41:33 +0900

    사용종료가 안되는 사용종료..

--- src/app/mypage/booking-item/booking-item.component.ts
+++ src/app/mypage/booking-item/booking-item.component.ts
@@ -16,6 +16,11 @@ export class BookingItemComponent implements OnInit {
     start: string;
     end: string;
 
+    changeTimeArray: number[];
+    curTime: number;
+    cnt:number;
+    bookingTime:string;
+
     constructor (private bookingService: BookingService) {
     }
 
@@ -34,6 +39,8 @@ export class BookingItemComponent implements OnInit {
         } else {
             this.timing = 'now';
         }
+
+        this.cnt = 0;
     }
 
     public cancel (bookingId: number) {
@@ -47,10 +54,31 @@ export class BookingItemComponent implements OnInit {
         });
     }
 
-    public endBooking(bookingId:number, endTime:string){
-        this.bookingService.end(bookingId, sessionStorage.getItem('id'),endTime).subscribe((res:any) => {
-            if ( res.status === 'success' ) {
-                this.needUpdate = true;
+    public endBooking(bookingId:number, bookingTime:string){
+        this.timeArray = this.bookingItem.booking_time.split(', ');
+        this.curTime = +moment().format('HH');
+        for(let i = 0; i <= this.timeArray.length; i++){
+            if(this.changeTimeArray[i] <= this.curTime){
+                //현재시간보다 같거나 작으면 changeTimeArray에 넣어요!
+                this.changeTimeArray[this.cnt] = this.timeArray[i];
+                this.cnt += 1;
+            }
+        }
+        for(let j = 0; j < this.cnt; j++){
+            //그리고 그 배열을 string변수에 넣으려고 했는데
+            //솔직히 이건 잘 모르겠다 에잇
+            if(j == 0){
+                this.bookingTime += this.changeTimeArray[j].toString();
+            }else{
+                this.bookingTime += ', ';
+                this.bookingTime += this.changeTimeArray[j].toString();
+            }             
+        }
+        this.bookingItem.change_time = this.bookingTime;
+
+        this.bookingService.end(bookingId, sessionStorage.getItem('id'),this.bookingItem.change_time).subscribe((res:any) => {
+           if ( res.status === 'success' ) {
+              this.needUpdate = true;
                 this.needUpdateOutput.emit(this.needUpdate);
             } else {
                 alert('[ERROR]: ' + res.result);
